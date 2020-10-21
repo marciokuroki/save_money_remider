a8c85f86e804a0582bffa54cc1ab0773fe3aa227
Marcio Kuroki <marciokuroki@gmail.com>
2020-10-21 00:37:15 -0300

    Refactoring the app to clean-dart architeture

--- lib/features/registration_goal/data/models/entry.dart
+++
@@ -1,9 +0,0 @@
-class Entry {
-  int id;
-  DateTime date;
-  double entryAmount;
-  bool done = false;
-  bool detached = false;
-
-  Entry(this.id, this.date, this.entryAmount, this.done, this.detached);
-}
